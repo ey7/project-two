19db0ed010c6ba982c96879eab1f56279d199ecc
ey7 <eoinyoung84@gmail.com>
2019-08-16 17:09:17 +0100

    scroll to top of page arrow added

--- scripts/script.js
+++ scripts/script.js
@@ -41,3 +41,22 @@ function search() {
 // add event listener to the clear variable to clear searchbox
 
 clear.addEventListener('click', clearInput);
+
+  // script for the scroll to top button
+
+  // When the user scrolls down 60px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.getElementById("scrollButton").style.display = "block";
+  } else {
+    document.getElementById("scrollButton").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topScroll() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
